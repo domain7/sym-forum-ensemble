3f715a50d2735c018ba55dbea9a9a1f1113c6bdf
Stephen Bau <bauhouse@gmail.com>
2010-03-07 22:34:51 -0800

    Remove .git directories from extensions to include them in repository

---
+++ extensions/asdc/extension.driver.php
@@ -0,0 +1,16 @@
+<?php
+
+	Class extension_ASDC extends Extension{
+
+		public function about(){
+			return array('name' => 'Advanced Symphony Database Connector (ASDC)',
+						 'version' => '1.3',
+						 'release-date' => '2010-02-12',
+						 'author' => array('name' => 'Alistair Kearney',
+										   'website' => 'http://pointybeard.com',
+										   'email' => 'alistair@pointybeard.com')
+				 		);
+		}
+		
+	}
+		
