ff2918fff03a02a8fb61fc83ddc2e338b601ea56
Stephen Bau <bauhouse@gmail.com>
2011-06-05 14:14:32 -0700

    Create Members section

--- install.sql
+++ install.sql
@@ -22,9 +22,10 @@ CREATE TABLE `tbl_fields_checkbox` (
   `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `field_id` (`field_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- *** DATA: `tbl_fields_checkbox` ***
+INSERT INTO `tbl_fields_checkbox` (`id`, `field_id`, `default_state`, `description`) VALUES (2, 11, 'off', 'Send me email when there is important news.');
 
 -- *** STRUCTURE: `tbl_fields_date` ***
 DROP TABLE IF EXISTS `tbl_fields_date`;
@@ -46,9 +47,95 @@ CREATE TABLE `tbl_fields_input` (
   `validator` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `field_id` (`field_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- *** DATA: `tbl_fields_input` ***
+INSERT INTO `tbl_fields_input` (`id`, `field_id`, `validator`) VALUES (5, 1, NULL);
+INSERT INTO `tbl_fields_input` (`id`, `field_id`, `validator`) VALUES (6, 7, '/^[^\\s:\\/?#]+:(?:\\/{2,3})?[^\\s.\\/?#]+(?:\\.[^\\s.\\/?#]+)*(?:\\/[^\\s?#]*\\??[^\\s?#]*(#[^\\s#]*)?)?$/');
+INSERT INTO `tbl_fields_input` (`id`, `field_id`, `validator`) VALUES (7, 8, NULL);
+INSERT INTO `tbl_fields_input` (`id`, `field_id`, `validator`) VALUES (8, 9, NULL);
+
+-- *** STRUCTURE: `tbl_fields_memberactivation` ***
+DROP TABLE IF EXISTS `tbl_fields_memberactivation`;
+CREATE TABLE `tbl_fields_memberactivation` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `field_id` int(11) unsigned NOT NULL,
+  `code_expiry` varchar(50) NOT NULL,
+  `activation_role_id` int(11) unsigned NOT NULL,
+  `deny_login` enum('yes','no') NOT NULL DEFAULT 'yes',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `field_id` (`field_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_fields_memberactivation` ***
+INSERT INTO `tbl_fields_memberactivation` (`id`, `field_id`, `code_expiry`, `activation_role_id`, `deny_login`) VALUES (2, 6, '1 hour', 1, 'no');
+
+-- *** STRUCTURE: `tbl_fields_memberemail` ***
+DROP TABLE IF EXISTS `tbl_fields_memberemail`;
+CREATE TABLE `tbl_fields_memberemail` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `field_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `field_id` (`field_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_fields_memberemail` ***
+INSERT INTO `tbl_fields_memberemail` (`id`, `field_id`) VALUES (2, 4);
+
+-- *** STRUCTURE: `tbl_fields_memberpassword` ***
+DROP TABLE IF EXISTS `tbl_fields_memberpassword`;
+CREATE TABLE `tbl_fields_memberpassword` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `field_id` int(11) unsigned NOT NULL,
+  `length` tinyint(2) NOT NULL,
+  `strength` enum('weak','good','strong') NOT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  `code_expiry` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `field_id` (`field_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_fields_memberpassword` ***
+INSERT INTO `tbl_fields_memberpassword` (`id`, `field_id`, `length`, `strength`, `salt`, `code_expiry`) VALUES (2, 3, 6, 'good', 'LXPj5GAJi6fWLY1K', '1 hour');
+
+-- *** STRUCTURE: `tbl_fields_memberrole` ***
+DROP TABLE IF EXISTS `tbl_fields_memberrole`;
+CREATE TABLE `tbl_fields_memberrole` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `field_id` int(11) unsigned NOT NULL,
+  `default_role` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `field_id` (`field_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_fields_memberrole` ***
+INSERT INTO `tbl_fields_memberrole` (`id`, `field_id`, `default_role`) VALUES (2, 5, 1);
+
+-- *** STRUCTURE: `tbl_fields_membertimezone` ***
+DROP TABLE IF EXISTS `tbl_fields_membertimezone`;
+CREATE TABLE `tbl_fields_membertimezone` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `field_id` int(11) unsigned NOT NULL,
+  `available_zones` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `field_id` (`field_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_fields_membertimezone` ***
+INSERT INTO `tbl_fields_membertimezone` (`id`, `field_id`, `available_zones`) VALUES (2, 10, 'AFRICA,AMERICA,ANTARCTICA,ARCTIC,ASIA,ATLANTIC,AUSTRALIA,EUROPE,INDIAN,PACIFIC');
+
+-- *** STRUCTURE: `tbl_fields_memberusername` ***
+DROP TABLE IF EXISTS `tbl_fields_memberusername`;
+CREATE TABLE `tbl_fields_memberusername` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `field_id` int(11) unsigned NOT NULL,
+  `validator` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `field_id` (`field_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_fields_memberusername` ***
+INSERT INTO `tbl_fields_memberusername` (`id`, `field_id`, `validator`) VALUES (2, 2, NULL);
 
 -- *** STRUCTURE: `tbl_fields_number` ***
 DROP TABLE IF EXISTS `tbl_fields_number`;
@@ -132,6 +219,168 @@ CREATE TABLE `tbl_fields_upload` (
 
 -- *** DATA: `tbl_fields_upload` ***
 
+-- *** STRUCTURE: `tbl_entries_data_1` ***
+DROP TABLE IF EXISTS `tbl_entries_data_1`;
+CREATE TABLE `tbl_entries_data_1` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `entry_id` int(11) unsigned NOT NULL,
+  `handle` varchar(255) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `entry_id` (`entry_id`),
+  KEY `handle` (`handle`),
+  KEY `value` (`value`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_entries_data_1` ***
+
+-- *** STRUCTURE: `tbl_entries_data_10` ***
+DROP TABLE IF EXISTS `tbl_entries_data_10`;
+CREATE TABLE `tbl_entries_data_10` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `entry_id` int(11) unsigned NOT NULL,
+  `handle` varchar(255) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `entry_id` (`entry_id`),
+  KEY `handle` (`handle`),
+  KEY `value` (`value`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_entries_data_10` ***
+
+-- *** STRUCTURE: `tbl_entries_data_11` ***
+DROP TABLE IF EXISTS `tbl_entries_data_11`;
+CREATE TABLE `tbl_entries_data_11` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `entry_id` int(11) unsigned NOT NULL,
+  `value` enum('yes','no') NOT NULL DEFAULT 'no',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `entry_id` (`entry_id`),
+  KEY `value` (`value`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_entries_data_11` ***
+
+-- *** STRUCTURE: `tbl_entries_data_2` ***
+DROP TABLE IF EXISTS `tbl_entries_data_2`;
+CREATE TABLE `tbl_entries_data_2` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `entry_id` int(11) unsigned NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`value`),
+  KEY `entry_id` (`entry_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_entries_data_2` ***
+
+-- *** STRUCTURE: `tbl_entries_data_3` ***
+DROP TABLE IF EXISTS `tbl_entries_data_3`;
+CREATE TABLE `tbl_entries_data_3` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `entry_id` int(11) unsigned NOT NULL,
+  `password` varchar(40) DEFAULT NULL,
+  `recovery-code` varchar(40) DEFAULT NULL,
+  `length` tinyint(2) NOT NULL,
+  `strength` enum('weak','good','strong') NOT NULL,
+  `reset` enum('yes','no') DEFAULT 'no',
+  `expires` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `recovery-code` (`recovery-code`),
+  KEY `entry_id` (`entry_id`),
+  KEY `length` (`length`),
+  KEY `password` (`password`),
+  KEY `expires` (`expires`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_entries_data_3` ***
+
+-- *** STRUCTURE: `tbl_entries_data_4` ***
+DROP TABLE IF EXISTS `tbl_entries_data_4`;
+CREATE TABLE `tbl_entries_data_4` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `entry_id` int(11) unsigned NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`value`),
+  KEY `entry_id` (`entry_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_entries_data_4` ***
+
+-- *** STRUCTURE: `tbl_entries_data_5` ***
+DROP TABLE IF EXISTS `tbl_entries_data_5`;
+CREATE TABLE `tbl_entries_data_5` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `entry_id` int(11) unsigned NOT NULL,
+  `role_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `entry_id` (`entry_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_entries_data_5` ***
+
+-- *** STRUCTURE: `tbl_entries_data_6` ***
+DROP TABLE IF EXISTS `tbl_entries_data_6`;
+CREATE TABLE `tbl_entries_data_6` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `entry_id` int(11) unsigned NOT NULL,
+  `activated` enum('yes','no') NOT NULL DEFAULT 'no',
+  `timestamp` datetime DEFAULT NULL,
+  `code` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `code` (`code`),
+  KEY `entry_id` (`entry_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_entries_data_6` ***
+
+-- *** STRUCTURE: `tbl_entries_data_7` ***
+DROP TABLE IF EXISTS `tbl_entries_data_7`;
+CREATE TABLE `tbl_entries_data_7` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `entry_id` int(11) unsigned NOT NULL,
+  `handle` varchar(255) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `entry_id` (`entry_id`),
+  KEY `handle` (`handle`),
+  KEY `value` (`value`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_entries_data_7` ***
+
+-- *** STRUCTURE: `tbl_entries_data_8` ***
+DROP TABLE IF EXISTS `tbl_entries_data_8`;
+CREATE TABLE `tbl_entries_data_8` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `entry_id` int(11) unsigned NOT NULL,
+  `handle` varchar(255) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `entry_id` (`entry_id`),
+  KEY `handle` (`handle`),
+  KEY `value` (`value`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_entries_data_8` ***
+
+-- *** STRUCTURE: `tbl_entries_data_9` ***
+DROP TABLE IF EXISTS `tbl_entries_data_9`;
+CREATE TABLE `tbl_entries_data_9` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `entry_id` int(11) unsigned NOT NULL,
+  `handle` varchar(255) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `entry_id` (`entry_id`),
+  KEY `handle` (`handle`),
+  KEY `value` (`value`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_entries_data_9` ***
+
 -- *** DATA: `tbl_entries` ***
 
 -- *** DATA: `tbl_extensions` ***
@@ -181,6 +430,17 @@ INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`
 INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (29, 13, '/blueprints/events/edit/', 'AppendEventFilter', 'appendFilter');
 
 -- *** DATA: `tbl_fields` ***
+INSERT INTO `tbl_fields` (`id`, `label`, `element_name`, `type`, `parent_section`, `required`, `sortorder`, `location`, `show_column`) VALUES (1, 'Name', 'name', 'input', 1, 'yes', 0, 'main', 'yes');
+INSERT INTO `tbl_fields` (`id`, `label`, `element_name`, `type`, `parent_section`, `required`, `sortorder`, `location`, `show_column`) VALUES (2, 'Username', 'username', 'memberusername', 1, 'yes', 1, 'main', 'yes');
+INSERT INTO `tbl_fields` (`id`, `label`, `element_name`, `type`, `parent_section`, `required`, `sortorder`, `location`, `show_column`) VALUES (3, 'Password', 'password', 'memberpassword', 1, 'yes', 2, 'main', 'no');
+INSERT INTO `tbl_fields` (`id`, `label`, `element_name`, `type`, `parent_section`, `required`, `sortorder`, `location`, `show_column`) VALUES (4, 'Email', 'email', 'memberemail', 1, 'yes', 3, 'main', 'yes');
+INSERT INTO `tbl_fields` (`id`, `label`, `element_name`, `type`, `parent_section`, `required`, `sortorder`, `location`, `show_column`) VALUES (5, 'Role', 'role', 'memberrole', 1, 'yes', 4, 'sidebar', 'yes');
+INSERT INTO `tbl_fields` (`id`, `label`, `element_name`, `type`, `parent_section`, `required`, `sortorder`, `location`, `show_column`) VALUES (6, 'Activation', 'activation', 'memberactivation', 1, 'no', 5, 'sidebar', 'no');
+INSERT INTO `tbl_fields` (`id`, `label`, `element_name`, `type`, `parent_section`, `required`, `sortorder`, `location`, `show_column`) VALUES (7, 'Website', 'website', 'input', 1, 'no', 6, 'main', 'no');
+INSERT INTO `tbl_fields` (`id`, `label`, `element_name`, `type`, `parent_section`, `required`, `sortorder`, `location`, `show_column`) VALUES (8, 'Location', 'location', 'input', 1, 'no', 7, 'sidebar', 'yes');
+INSERT INTO `tbl_fields` (`id`, `label`, `element_name`, `type`, `parent_section`, `required`, `sortorder`, `location`, `show_column`) VALUES (9, 'City', 'city', 'input', 1, 'no', 8, 'sidebar', 'yes');
+INSERT INTO `tbl_fields` (`id`, `label`, `element_name`, `type`, `parent_section`, `required`, `sortorder`, `location`, `show_column`) VALUES (10, 'Timezone', 'timezone', 'membertimezone', 1, 'no', 9, 'sidebar', 'no');
+INSERT INTO `tbl_fields` (`id`, `label`, `element_name`, `type`, `parent_section`, `required`, `sortorder`, `location`, `show_column`) VALUES (11, 'Email Opt-in', 'email-opt-in', 'checkbox', 1, 'no', 10, 'sidebar', 'yes');
 
 -- *** DATA: `tbl_forum_read_discussions` ***
 
@@ -196,5 +456,6 @@ INSERT INTO `tbl_members_roles` (`id`, `name`, `handle`) VALUES (1, 'Public', 'p
 -- *** DATA: `tbl_pages_types` ***
 
 -- *** DATA: `tbl_sections` ***
+INSERT INTO `tbl_sections` (`id`, `name`, `handle`, `sortorder`, `entry_order`, `entry_order_direction`, `hidden`, `navigation_group`) VALUES (1, 'Members', 'members', 1, NULL, 'asc', 'no', 'Forum');
 
 -- *** DATA: `tbl_sections_association` ***
